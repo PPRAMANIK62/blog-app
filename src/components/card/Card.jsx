01f617c13da4ddc2d5eebe859b8048404147db81
Purbayan Pramanik <purbayanpramanik62@gmail.com>
2024-05-19 22:43:33 +0530

    FETCH: fetching posts with category done

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -6,9 +6,11 @@ import Link from "next/link";
 const Card = ({ key, item }) => {
   return (
     <div className={styles.container} key={key}>
-      <div className={styles.imageContainer}>
-        <Image src="/p1.jpeg" alt="" fill className={styles.image} />
-      </div>
+      {item.img && (
+        <div className={styles.imageContainer}>
+          <Image src={item.img} alt="" fill className={styles.image} />
+        </div>
+      )}
       <div className={styles.textContainer}>
         <div className={styles.detail}>
           <span className={styles.date}>
@@ -16,16 +18,11 @@ const Card = ({ key, item }) => {
           </span>
           <span className={styles.category}>{item.catSlug}</span>
         </div>
-        <Link href="/">
-          <h1>{item.title}</h1>
+        <Link href={`/posts/${item.slug}`}>
+          <h1>{item.title.substring(0, 60)}</h1>
         </Link>
-        <p className={styles.desc}>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor,
-          cupiditate eum veritatis odit aliquam suscipit at necessitatibus
-          ducimus! Harum, asperiores rerum esse facilis ratione sapiente maxime
-          eius doloremque tempora consectetur.
-        </p>
-        <Link href="/" className={styles.link}>
+        <p className={styles.desc}>{item.desc}</p>
+        <Link href={`/posts/${item.slug}`} className={styles.link}>
           Read More
         </Link>
       </div>
